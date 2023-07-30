25c8a6d2042b7cba79e2c71a54952b261ddb58e5
Jomin Rajesh K <Jomin.Rajesh.K1@ibm.com>
2023-07-30 08:53:26 +0530

    added cors

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors')
 const port = parseInt( process.env.PORT || '3000', 10);
 const addProxyMiddleware = require('./proxyMiddleware.js');
 const app = express();
+app.use(cors())
 addProxyMiddleware(app);
 const customHost = process.env.HOST;
 const host = customHost || null; 
